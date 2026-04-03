75ebf3f06c614388a0d0b3d4af8d2e44f90c15f6
hyEOnjuNg0-0 <117015040+hyEOnjuNg0-0@users.noreply.github.com>
2026-04-03 20:05:53 +0900

    db/scheme cocktails, reviews 추가

---
+++ backend/app/db/scheme/reviews.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class ReviewBase(BaseModel):
+    title:str
+    description:str|None=None
+
+class ReviewCreate(ReviewBase):
+    pass
+
+class ReviewInDB(ReviewBase):
+    review_id:int
+    user_id:int
+    cocktail_id:int
+    created_at:datetime
+
+    class Config:
+        from_attributes = True
+
+class ReviewRead(ReviewBase):
+    pass
