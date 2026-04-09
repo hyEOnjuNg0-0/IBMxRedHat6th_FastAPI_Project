57dc0f33cdf5ad50974ad575d1491061e3cfce6b
soyoon <soy6215@naver.com>
2026-04-09 12:32:00 +0900

    cocktails scheme edit2

--- backend/app/db/scheme/cocktails.py
+++ backend/app/db/scheme/cocktails.py
@@ -8,29 +8,17 @@ class CocktailCreate(CocktailBase):
     cocktail_detail:str
 
 class CocktailUpdate(BaseModel):
-    cocktail_name:str
-    cocktail_base:str
-    cocktail_detail:str
+    cocktail_name:str | None = None    
+    cocktail_base:str | None = None
+    cocktail_detail:str | None = None
 
-class CocktailInDB(CocktailBase):
+class CocktailListRead(CocktailBase):
     cocktail_id: int
+
+    class Config:
+        from_attributes = True
+
+
+class CocktailDetail(CocktailListRead):
     cocktail_base: str
     cocktail_detail: str
-
-    class Config:
-        from_attributes = True
-
-
-class CocktailRead(CocktailBase):
-    cocktail_id:int
-
-    class Config:
-        from_attributes = True
-
-class CocktailDetail(CocktailBase):
-    cocktail_id: int
-    cocktail_base: str
-    cocktail_detail: str
-
-    class Config:
-        from_attributes = True
